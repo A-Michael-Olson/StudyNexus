bc8ccb931f82f5aab6aba31d8c0a13b502719a13
AMichaelOlson <anthonymolson@byui.edu>
2026-02-17 13:40:26 -0700

    foreign key needed renaming

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -79,7 +79,7 @@ async function loadGroupMembers(groupId) {
         .from("group_members")
         .select(`
             user_id,
-            profiles!group_members_user_id_fkey1 (
+            profiles!group_members_user_id_fkey (
                 username
             )
         `)
