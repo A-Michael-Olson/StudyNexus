d5e8a945150708d5c984ba3442df31c31e823233
AMichaelOlson <anthonymolson@byui.edu>
2026-03-09 13:01:23 -0600

    okay, added an extra pages link

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -85,7 +85,7 @@ async function loadGroupMembers(groupId) {
         userEl.classList.add("user-item");
 
         if (isCurrentUser) {
-            userEl.href = "../pages/login/profilepage.html";
+            userEl.href = "../login/profilepage.html";
             userEl.classList.add("self");
         }
 
