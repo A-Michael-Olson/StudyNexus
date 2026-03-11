3f392109337b8acb2e0e338db9aba0a72593de59
AMichaelOlson <anthonymolson@byui.edu>
2026-03-11 14:37:58 -0600

    Character Issue

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -10,7 +10,7 @@ export function updateHeaderChannel(channelName) {
 
     const groupName = navName.dataset.groupName;
 
-    navName.textContent = `${groupName} — #${channelName}`;
+    navName.textContent = `${groupName} - #${channelName}`;
 }
 
 async function loadDashboard() {
